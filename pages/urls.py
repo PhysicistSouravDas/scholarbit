fddb20c04a5c2c54663fba32a061bee937c545d5
PhysicistSouravDas <souravdas.sd20@gmail.com>
2020-08-24 23:53:24 +0530

    Added minimal search feature :sparkles:

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, FeaturesPageView, AnnouncementsPageView
+from .views import HomePageView, AboutPageView, ContactPageView, FeaturesPageView, AnnouncementsPageView, SearchResultsView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name = 'home'),
     path('about/', AboutPageView.as_view(), name = 'about'),
     path('contact/', ContactPageView.as_view(), name = 'contact'),
     path('features/', FeaturesPageView.as_view(), name = 'features'),
-    path('announcements/', AnnouncementsPageView.as_view(), name = 'announcements')
+    path('announcements/', AnnouncementsPageView.as_view(), name = 'announcements'),
+    #Search URL
+    path("search/", SearchResultsView.as_view(), name="search_results"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+# Importing Articles model for search view
+from articles.models import Article
+from django.db.models import Q
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -16,3 +19,17 @@ class FeaturesPageView(TemplateView):
 
 class AnnouncementsPageView(TemplateView):
     template_name = 'announcements.html'
+
+#Search View Below
+class SearchResultsView(ListView):
+    model = Article
+    template_name = "search_results.html"
+    # The template is in users/templates/search_results.html
+
+    def get_context_data(self):
+        query = self.request.GET.get('q')
+        search_list = Article.objects.filter(
+            Q(title__icontains=query) | Q(body__icontains=query)
+            )
+        ctx = {'search_list': search_list}
+        return ctx
