33ab50c7b6f9de00026ea85cbdc7184062242076
PhysicistSouravDas <souravdas.sd20@gmail.com>
2020-08-25 23:44:10 +0530

    Added functionality to search for users :zap:

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
-# Importing Articles model for search view
+# Importing Articles and User model for search view
 from articles.models import Article
+from django.contrib.auth import get_user_model
 from django.db.models import Q
 
 # Create your views here.
@@ -21,6 +22,7 @@ class AnnouncementsPageView(TemplateView):
     template_name = 'announcements.html'
 
 #Search View Below
+Users = get_user_model()
 class SearchResultsView(ListView):
     model = Article
     template_name = "search_results.html"
@@ -28,8 +30,28 @@ class SearchResultsView(ListView):
 
     def get_context_data(self):
         query = self.request.GET.get('q')
-        search_list = Article.objects.filter(
-            Q(title__icontains=query) | Q(body__icontains=query)
+        if '@' in query:
+            modified_query = query[1:]
+            if ' '  in modified_query:
+                queried_first_name = modified_query[:modified_query.index(' ')]
+                queried_last_name =  modified_query[modified_query.index(' ') + 1:]
+                search_list = Users.objects.filter(
+                    Q(first_name__icontains = queried_first_name) | 
+                    Q(last_name__icontains = queried_last_name)
+                )
+            else:
+                search_list = Users.objects.filter(
+                    Q(first_name__icontains = modified_query) | 
+                    Q(last_name__icontains = modified_query) | 
+                    Q(username__icontains = modified_query)
             )
-        ctx = {'search_list': search_list}
+        else:
+            search_list = Article.objects.filter(
+                Q(title__icontains=query) | Q(body__icontains=query)
+                )
+        ctx = {
+            'search_list': search_list, 
+            'query':  query, 
+            'search_count': search_list.count()
+            }
         return ctx
